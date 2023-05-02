781c3e825fda17ec4ee332eb795731f1b6ac89c9
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-02 11:52:54 +0800

    transfer example to array folder

--- Object Orientation/src/com/java/objects/arrays/arrays.java
+++ Object Orientation/src/com/java/objects/arrays/arrays.java
@@ -272,6 +272,63 @@ public class arrays {
       }
     }
 
+    // 2. Student Scores - 3D Jagged Array Example:
+    // Creating 3D Jagged Array for student scores - 3 colleges:
+    int[][][] jaggedArrayScores2 = new int[3][][];
+
+    // For determining the number of classes for each college:
+    jaggedArrayScores2[0] = new int[3][];
+    jaggedArrayScores2[1] = new int[2][];
+    jaggedArrayScores2[2] = new int[4][];
+
+    // Determining the number of students in each class:
+    // College A:
+    jaggedArrayScores2[0][0] = new int[3];
+    jaggedArrayScores2[0][1] = new int[2];
+    jaggedArrayScores2[0][2] = new int[4];
+
+    // College B:
+    jaggedArrayScores2[1][0] = new int[5];
+    jaggedArrayScores2[1][1] = new int[3];
+
+    // College C:
+    jaggedArrayScores2[2][0] = new int[1];
+    jaggedArrayScores2[2][1] = new int[4];
+    jaggedArrayScores2[2][2] = new int[2];
+    jaggedArrayScores2[2][3] = new int[3];
+
+    System.out.println("Enter the scores of all students:");
+    System.out.println();
+
+    for (int i = 0; i < jaggedArrayScores2.length; i++) {
+      for (int j = 0; j < jaggedArrayScores2[i].length; j++) {
+        for (int k = 0; k < jaggedArrayScores2[i][j].length; k++) {
+          System.out.println(
+              "Enter score for - College " + (i + 1) +
+                  ", Class " + (j + 1) +
+                  ", Student " + (k + 1) +
+                  ":");
+          jaggedArrayScores2[i][j][k] = scan.nextInt();
+        }
+
+      }
+    }
+
+    System.out.println();
+
+    // Printing the scores of all students:
+    for (int i = 0; i < jaggedArrayScores2.length; i++) {
+      for (int j = 0; j < jaggedArrayScores2[i].length; j++) {
+        for (int k = 0; k < jaggedArrayScores2[i][j].length; k++) {
+          System.out.println(
+              "For College " + (i + 1) +
+                  ", Class " + (j + 1) +
+                  ", Student " + (k + 1) +
+                  ", the score is: " + jaggedArrayScores2[i][j][k] + ".");
+        }
+      }
+    }
+
     scan.close();
   }
 }
