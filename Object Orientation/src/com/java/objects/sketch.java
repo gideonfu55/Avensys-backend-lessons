c9ed46c4c22d52f6d776d9071d688d7be81f59fc
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-02 11:30:04 +0800

    update 3D jagged array for student scores

--- Object Orientation/src/com/java/objects/sketch.java
+++ Object Orientation/src/com/java/objects/sketch.java
@@ -7,43 +7,43 @@ public class sketch {
 
     Scanner scan = new Scanner(System.in);
 
-    int[][][] jaggedArrayBank = new int[3][][];
+    // Creating 3D Jagged Array for student scores - 3 colleges:
+    int[][][] jaggedArrayScores2 = new int[3][][];
 
-    // For determining the number of bank branches for each bank:
-    jaggedArrayBank[0] = new int[3][];
-    jaggedArrayBank[1] = new int[4][];
-    jaggedArrayBank[2] = new int[2][];
+    // For determining the number of classes for each college:
+    jaggedArrayScores2[0] = new int[3][];
+    jaggedArrayScores2[1] = new int[2][];
+    jaggedArrayScores2[2] = new int[4][];
 
-    // Determining the number of customers in each branch:
-    // Bank A:
-    jaggedArrayBank[0][0] = new int[4];
-    jaggedArrayBank[0][1] = new int[2];
-    jaggedArrayBank[0][2] = new int[3];
+    // Determining the number of students in each class:
+    // College A:
+    jaggedArrayScores2[0][0] = new int[3];
+    jaggedArrayScores2[0][1] = new int[2];
+    jaggedArrayScores2[0][2] = new int[4];
 
-    // Bank B:
-    jaggedArrayBank[1][0] = new int[1];
-    jaggedArrayBank[1][1] = new int[2];
-    jaggedArrayBank[1][2] = new int[4];
-    jaggedArrayBank[1][3] = new int[2];
+    // College B:
+    jaggedArrayScores2[1][0] = new int[5];
+    jaggedArrayScores2[1][1] = new int[3];
 
-    // Bank C:
-    jaggedArrayBank[2][0] = new int[5];
-    jaggedArrayBank[2][1] = new int[2];
+    // College C:
+    jaggedArrayScores2[2][0] = new int[1];
+    jaggedArrayScores2[2][1] = new int[4];
+    jaggedArrayScores2[2][2] = new int[2];
+    jaggedArrayScores2[2][3] = new int[3];
 
-    // In this case, there are different sizes of each bank account:
-    System.out.println("Enter the account balance of all bank customers:");
+    System.out.println("Enter the scores of all students:");
     System.out.println();
 
-    for (int i = 0; i < jaggedArrayBank.length; i++) {
-      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
-        for (int k = 0; k < jaggedArrayBank[i][j].length; k++) {
+    for (int i = 0; i < jaggedArrayScores2.length; i++) {
+      for (int j = 0; j < jaggedArrayScores2[i].length; j++) {
+        for (int k = 0; k < jaggedArrayScores2[i][j].length; k++) {
           System.out.println(
-            "Please enter balance for - Bank " + (i + 1) + 
-            ", Branch " + (j + 1) + 
-            ", Customer " + (k + 1) + 
+            "Please enter balance for - College " + (i + 1) + 
+            ", Class " + (j + 1) +
+            ", Student " + (k + 1) +
             ":"
           );
-          jaggedArrayBank[i][j][k] = scan.nextInt();
+          jaggedArrayScores2[i][j][k] = scan.nextInt();
         }
 
       }
@@ -52,14 +52,14 @@ public class sketch {
     System.out.println();
 
     // Printing the account balances of customers:
-    for (int i = 0; i < jaggedArrayBank.length; i++) {
-      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
-        for (int k = 0; k < jaggedArrayBank[i][j].length; k++) {
+    for (int i = 0; i < jaggedArrayScores2.length; i++) {
+      for (int j = 0; j < jaggedArrayScores2[i].length; j++) {
+        for (int k = 0; k < jaggedArrayScores2[i][j].length; k++) {
           System.out.println(
-            "For Bank " + (i + 1) + 
-            ", Branch " + (j + 1) +
-            ", Customer " + (k + 1) + 
-            ", the account balance is: $" + jaggedArrayBank[i][j][k] + "."
+            "For College " + (i + 1) + 
+            ", Class " + (j + 1) +
+            ", Student " + (k + 1) + 
+            ", the score is: " + jaggedArrayScores2[i][j][k] + "."
           );
         }
       }
