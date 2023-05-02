e39f6a2195ec0badc190aa4adeea6847d158bae0
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-02 10:21:23 +0800

    update bank account 2D array in array lesson folder

--- Object Orientation/src/com/java/objects/arrays/arrays.java
+++ Object Orientation/src/com/java/objects/arrays/arrays.java
@@ -146,6 +146,37 @@ public class arrays {
       }
     }
 
+    // ++++++++++++++++++++++++++++++++
+    // Bank account example - 2D Array:
+    // ++++++++++++++++++++++++++++++++
+    int[][] jaggedArrayBank = new int[5][];
+    jaggedArrayBank[0] = new int[3];
+    jaggedArrayBank[1] = new int[1];
+    jaggedArrayBank[2] = new int[4];
+    jaggedArrayBank[3] = new int[5];
+    jaggedArrayBank[4] = new int[2];
+
+    // In this case, there are different sizes of each bank account:
+    System.out.println("Enter the account balance of all bank customers:");
+    System.out.println();
+
+    for (int i = 0; i < jaggedArrayBank.length; i++) {
+      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
+        System.out.println("Please enter balance for - Bank " + (i + 1) + ", Customer " + (j + 1) + ":");
+        jaggedArrayBank[i][j] = scan.nextInt();
+      }
+    }
+
+    System.out.println();
+
+    // Printing the account balances of customers:
+    for (int i = 0; i < jaggedArrayBank.length; i++) {
+      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
+        System.out.println("For Bank " + (i + 1) + ", Customer " + (j + 1) + ", the account balance is: $"
+            + jaggedArrayBank[i][j] + ".");
+      }
+    }
+
     scan.close();
   }
 }
--- Object Orientation/src/com/java/objects/sketch.java
+++ Object Orientation/src/com/java/objects/sketch.java
@@ -6,33 +6,6 @@ public class sketch {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-
-    int[][] jaggedArrayBank = new int[5][];
-    jaggedArrayBank[0] = new int[3];
-    jaggedArrayBank[1] = new int[1];
-    jaggedArrayBank[2] = new int[4];
-    jaggedArrayBank[3] = new int[5];
-    jaggedArrayBank[4] = new int[2];
-
-    // In this case, there may be different sizes of each bank account:
-    System.out.println("Enter the account balance of all bank customers:");
-    System.out.println();
-
-    for (int i = 0; i < jaggedArrayBank.length; i++) {
-      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
-        System.out.println("Please enter balance for - Bank " + (i + 1) + ", Customer " + (j + 1) + ":");
-        jaggedArrayBank[i][j] = scan.nextInt();
-      }
-    }
-
-    System.out.println();
-
-    // Printing the account balances of customers:
-    for (int i = 0; i < jaggedArrayBank.length; i++) {
-      for (int j = 0; j < jaggedArrayBank[i].length; j++) {
-        System.out.println("For Bank " + (i + 1) + ", Customer " + (j + 1) + ", the account balance is: $" + jaggedArrayBank[i][j] + ".");
-      }
-    }
     
     scan.close();
 
