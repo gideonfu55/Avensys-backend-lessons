86a27907056497b4c83e51a505c075b817ee2ea1
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-03 18:09:45 +0800

    include test file

---
+++ Object Orientation/src/com/java/objects/Testing1.java
@@ -0,0 +1,11 @@
+package com.java.objects;
+
+// public class Example {
+  
+// }
+
+public class Testing1 {
+  public static void main(String[] args) {
+    System.out.println("Hello Boss!");
+  }
+}
