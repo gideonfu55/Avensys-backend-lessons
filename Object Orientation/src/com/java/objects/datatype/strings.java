a2b0832bbd521d618a2fa7ebeeb1215dc5994f52
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-03 11:02:44 +0800

    update Strings lesson

--- Object Orientation/src/com/java/objects/datatype/strings.java
+++ Object Orientation/src/com/java/objects/datatype/strings.java
@@ -1,5 +1,18 @@
 package com.java.objects.datatype;
 
 public class strings {
+  public static void main(String[] args) {
+    
+    String s1 = new String("Gideon");
+    String s2 = new String("Gideon");
+    
+    // Understanding how the 2 variables are reference pointers that point to the same object:
+    if (s1.equals(s2)) {
+      System.out.println("References are equal.");
+    } else {
+      System.out.println("References are not equal.");
+    }
+
+  }
   
 }
