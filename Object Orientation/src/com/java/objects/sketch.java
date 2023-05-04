d792093badf5c72a51c8f7fd65e08b9ecd9bab42
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-04 10:14:51 +0800

    lesson on StringBuffer and StringBuilder

--- Object Orientation/src/com/java/objects/sketch.java
+++ Object Orientation/src/com/java/objects/sketch.java
@@ -12,8 +12,30 @@ public class sketch {
       System.out.println("Strings are unequal");
     }
 
+    /**
+     * Mutable String Types: 
+     * StringBuffer
+     */
     StringBuffer sb = new StringBuffer("This is");
+    sb.append(" a Java Class. Like the object. Not the classroom. Nvm.");
     System.out.println(sb);
+
+    // In StringBuffer, the default starting capacity is 16.
+    StringBuffer sbCapacity = new StringBuffer();
+    System.out.println(sbCapacity.capacity());
+
+    // If more characters are added, the StringBuffer will automatically resize itself to accommodate the extra storage required.
+    System.out.println(sb.capacity()); // this is exactly the size of the number of chars in the new StringBuffer
+
+    /*
+     * StringBuilder
+     */
+
+    StringBuilder sbuild = new StringBuilder("This is");
+    sbuild.append(" a Java Class. Like the object. Not the classroom. Nvm.");
+
+    System.out.println(sbuild);
+    System.out.println(sbuild.capacity());
 
   }
 }
