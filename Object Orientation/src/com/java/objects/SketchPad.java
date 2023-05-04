8e565dacbe1828cbbe522e282458022c4d4a7141
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-04 12:26:20 +0800

    add methods lesson

--- Object Orientation/src/com/java/objects/SketchPad.java
+++ Object Orientation/src/com/java/objects/SketchPad.java
@@ -1,9 +1,12 @@
 package com.java.objects;
 
+import com.java.objects.methods.Calculator;
+
 public class SketchPad {
   public static void main(String[] args) {
 
-    
+    Calculator myCal = new Calculator();
+    myCal.add();
 
   }
 }
--- Object Orientation/src/com/java/objects/methods/Calculator.java
+++ Object Orientation/src/com/java/objects/methods/Calculator.java
@@ -1,5 +1,12 @@
 package com.java.objects.methods;
 
 public class Calculator {
-  
+  // Type  1: no input and no output - add() method
+  public void add () {
+    int a = 10;
+    int b = 30;
+    int c = a + b;
+    System.out.println(c);
+  }
+
 }
