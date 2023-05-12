4f1f2011baab55a3b22812684ae78b1f2413e7b3
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-12 10:58:21 +0800

    update inheritance lesson - overriding and specialized methods

--- Object Orientation/src/com/java/objects/classes/inheritance/overriding.java
+++ Object Orientation/src/com/java/objects/classes/inheritance/overriding.java
@@ -1,5 +1,31 @@
 package com.java.objects.classes.inheritance;
 
-public class overriding {
+class Teacher {
+  void markAttendance() {
+    System.out.println("Teacher is marking attendance.");
+  }
+
+  void teach() {
+    System.out.println("Teacher is teaching");
+  }
+}
+
+class PhysicsTeacher extends Teacher {
+  void teach() {
+    System.out.println("Physics teacher is teaching physics.");
+  }
+
+  void labExpPhy() {
+    System.out.println("Physics teacher is doing a lab experiment.");
+  }
+}
+
+public class overriding extends Object {
+  public static void main(String[] args) {
+    PhysicsTeacher pt1 = new PhysicsTeacher();
+    pt1.markAttendance();
+    pt1.teach();
+    pt1.labExpPhy();
+  }
   
 }
--- Object Orientation/src/sketch.java
+++ Object Orientation/src/sketch.java
@@ -1,18 +1,5 @@
-
-class DemoOne {
-  public DemoOne() {
-  }
-}
-
-class DemoTwo extends DemoOne {
-  public DemoTwo() {
-    super();
-  }
-}
-
 public class sketch extends Object {
   public static void main(String[] args) {
-    DemoTwo d2 = new DemoTwo();
-    
+
   }
 }
