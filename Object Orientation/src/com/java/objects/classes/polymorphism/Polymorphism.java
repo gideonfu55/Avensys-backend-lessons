c7f805e8654a1fbdb6af609c1d398b99f861d7eb
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-12 14:48:40 +0800

    update Polymorphism lesson

--- Object Orientation/src/com/java/objects/classes/polymorphism/Polymorphism.java
+++ Object Orientation/src/com/java/objects/classes/polymorphism/Polymorphism.java
@@ -4,11 +4,19 @@ class Teacher {
   void teach() {
     System.out.println("Teacher is teaching");
   }
+
+  void takeAttendance() {
+    System.out.println("Teacher is taking attendance.");
+  }
 }
 
 class PhysicsTeacher extends Teacher {
-  void teach() {
-    System.out.println("Physics teacher is teaching physics.");
+  // void teach() {
+  //   System.out.println("Physics teacher is teaching physics.");
+  // }
+
+  void phyLabExp() {
+    System.out.println("Physics teacher is conducting a Physics experiment.");
   }
 }
 
@@ -26,11 +34,16 @@ class BiologyTeacher extends Teacher {
 
 public class Polymorphism {
   public static void main(String[] args) {
-    PhysicsTeacher pt1 = new PhysicsTeacher();
+    PhysicsTeacher pt1 = new PhysicsTeacher(); // this is known as tight coupling
     ChemistryTeacher ct1 = new ChemistryTeacher();
     BiologyTeacher bt1 = new BiologyTeacher();
     pt1.teach(); // 1:1
     ct1.teach(); // 1:1
     bt1.teach(); // 1:1
+    Teacher ref;
+    ref = pt1;
+    ref.teach(); 
+    ref.takeAttendance(); // pt is taking 2 forms now: Teacher and PhysicsTeacher
+    // ref.phyLabExp(); // This is not possible, because the parent cannot use the methods of the child.
   }
 }
