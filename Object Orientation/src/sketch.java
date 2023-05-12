9b665c0665775688273d7104d721259e6be1792b
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-12 10:17:05 +0800

    update inheritance lesson

--- Object Orientation/src/sketch.java
+++ Object Orientation/src/sketch.java
@@ -1,21 +1,21 @@
 
-class DemoOne extends Object {
-  int a = 10;
-}
-
-class DemoTwo extends DemoOne {
-  public void greet() {
-    System.out.println("Hello team from DemoTwo!");
+class DemoOne {
+  public DemoOne() {
   }
 }
 
-class DemoThree extends DemoTwo {
-
+class DemoTwo extends DemoOne {
+  public DemoTwo() {
+    super();
+  }
 }
+
+// class DemoThree extends DemoTwo {
+
+// }
 
 public class sketch extends Object {
   public static void main(String[] args) {
-    DemoThree d3 = new DemoThree();
-    System.out.println(d3.a);
+    DemoTwo d2 = new DemoTwo();
   }
 }
