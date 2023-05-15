d9d47bdf7f2cacfccae449a3943e084d87cf82fb
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-15 10:39:09 +0800

    update interface lesson example

--- Object Orientation/src/com/java/objects/classes/interfaces/InterfaceLesson.java
+++ Object Orientation/src/com/java/objects/classes/interfaces/InterfaceLesson.java
@@ -1,7 +1,90 @@
 package com.java.objects.classes.interfaces;
 
-public class InterfaceLesson {
+import java.util.Scanner;
+
+interface Calculator {
+  public void multiply();
+  public void divide();
+}
+
+class MyCal1 implements Calculator {
+
+  public void multiply() {
+    int a = 10;
+    int b = 20;
+    int c = a * b;
+    System.out.println(c);
+  }
+
+  public void divide() {
+    int a = 10;
+    int b = 5;
+    int c = a / b;
+    System.out.println(c);
+  }
+}
+
+class MyCal2 implements Calculator {
+
+  public void multiply() {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = a * b;
+    System.out.println(c);
+  }
+
+  public void divide() {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = a / b;
+    System.out.println(c);
+  }
+
+  class MyCal3 implements Calculator {
+
+    public void multiply() {
+      Scanner scan = new Scanner(System.in);
+      int a = scan.nextInt();
+      int b = scan.nextInt();
+      int c;
+      if (a == 0 || b == 0) {
+        c = 0;
+      } else {
+        c = a * b;
+      }
+
+      System.out.println(c);
+    }
+
+    public void divide() {
+      Scanner scan = new Scanner(System.in);
+      int a = scan.nextInt();
+      int b = scan.nextInt();
+      int c = 0;
+      if (a == 0 || b == 0) {
+        System.out.println("Please provide non-zero integers.");;
+      } else {
+        c = a / b;
+      }
+
+      System.out.println(c);
+    }
+  }
+
+  class MathOperation {
+    public void acceptCalculator(Calculator ref) {
+      ref.multiply();
+      ref.divide();
+    }
+  }
+  
+}
+
+public class InterfaceLesson extends Object {
   public static void main(String[] args) {
     
   }
+
 }
