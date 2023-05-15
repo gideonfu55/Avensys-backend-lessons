5cfe4209f7a0a07982435315296b0d27e6201efe
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-15 13:08:54 +0800

    update assignment solution

--- Object Orientation/src/sketch.java
+++ Object Orientation/src/sketch.java
@@ -10,6 +10,7 @@ class businessMan1 implements CalculateInterest {
 
   int loanAmount = 1000;
   int interestRate = 2;
+  int paymentPeriod = 2;
   int simpleInterest = 0;
 
   public void acceptInput() {
@@ -17,7 +18,7 @@ class businessMan1 implements CalculateInterest {
   }
 
   public void findSimpleInterest() {
-    simpleInterest = loanAmount * interestRate / 100;
+    simpleInterest = loanAmount * interestRate / 100 / paymentPeriod;
     System.out.println(simpleInterest);
   }
 
@@ -31,17 +32,20 @@ class businessMan2 implements CalculateInterest {
 
   int loanAmount = 0;
   int interestRate = 3;
+  int paymentPeriod = 0;
   int simpleInterest = 0;
 
   public void acceptInput() {
     Scanner scan = new Scanner(System.in);
-    System.out.println("\nPlease provide your required loan amount:");
+    System.out.println("Please provide your required loan amount:");
     loanAmount = scan.nextInt();
-    System.out.println("Your loan amount is: $" + loanAmount);
+    System.out.println("Please provide the intended payment period:");
+    paymentPeriod = scan.nextInt();
+    System.out.println("Your loan amount is: $" + loanAmount + "over " + paymentPeriod + " years.");
   }
 
   public void findSimpleInterest() {
-    simpleInterest = loanAmount * interestRate;
+    simpleInterest = loanAmount * interestRate / 100 / paymentPeriod;
     System.out.println(simpleInterest);
   }
 
@@ -54,23 +58,27 @@ class businessMan2 implements CalculateInterest {
 class businessMan3 implements CalculateInterest {
   int loanAmount = 0;
   int interestRate = 4;
+  int paymentPeriod = 0;
   int simpleInterest = 0;
 
   public void acceptInput() {
     Scanner scan = new Scanner(System.in);
-    System.out.println("\nPlease provide your required loan amount:");
+    System.out.println("Please provide your required loan amount:");
     loanAmount = scan.nextInt();
+    System.out.println("Please provide the intended payment period:");
+    paymentPeriod = scan.nextInt();
 
-    if (loanAmount <= 0) {
-      System.out.println("Please enter a valid loan amount.");
+    if (loanAmount <= 0 || paymentPeriod <= 0) {
+      System.out.println("Please enter a valid loan amount and payment period > 0.");
       loanAmount = 0;
+      paymentPeriod = 0;
     } else {
-      System.out.println("Your loan amount is: $" + loanAmount);
+      System.out.println("Your loan amount is: $" + loanAmount + "over " + paymentPeriod + " years.");
     }
   }
 
   public void findSimpleInterest() {
-    simpleInterest = loanAmount * interestRate;
+    simpleInterest = loanAmount * interestRate / 100 / paymentPeriod;
     System.out.println(simpleInterest);
   }
 
@@ -96,12 +104,15 @@ public class sketch {
 
     BankTwo bankNew = new BankTwo();
     System.out.println("Business Man 1: ");
+    System.out.println("===============");
     bankNew.acceptCalculateInterest(b1);
 
     System.out.println("\nBusiness Man 2: ");
+    System.out.println("===============");
     bankNew.acceptCalculateInterest(b2);
 
     System.out.println("\nBusiness Man 3: ");
+    System.out.println("===============");
     bankNew.acceptCalculateInterest(b3);
   }
 }
