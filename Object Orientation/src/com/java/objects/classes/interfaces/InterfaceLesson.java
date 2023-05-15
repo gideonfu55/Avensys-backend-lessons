b781b5edac9bbfa028422f59799f1dc88e393f33
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-15 10:44:10 +0800

    update interface class example

--- Object Orientation/src/com/java/objects/classes/interfaces/InterfaceLesson.java
+++ Object Orientation/src/com/java/objects/classes/interfaces/InterfaceLesson.java
@@ -41,50 +41,58 @@ class MyCal2 implements Calculator {
     int c = a / b;
     System.out.println(c);
   }
+}
 
-  class MyCal3 implements Calculator {
+class MyCal3 implements Calculator {
 
-    public void multiply() {
-      Scanner scan = new Scanner(System.in);
-      int a = scan.nextInt();
-      int b = scan.nextInt();
-      int c;
-      if (a == 0 || b == 0) {
-        c = 0;
-      } else {
-        c = a * b;
-      }
-
-      System.out.println(c);
+  public void multiply() {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c;
+    if (a == 0 || b == 0) {
+      c = 0;
+    } else {
+      c = a * b;
     }
 
-    public void divide() {
-      Scanner scan = new Scanner(System.in);
-      int a = scan.nextInt();
-      int b = scan.nextInt();
-      int c = 0;
-      if (a == 0 || b == 0) {
-        System.out.println("Please provide non-zero integers.");;
-      } else {
-        c = a / b;
-      }
-
-      System.out.println(c);
-    }
+    System.out.println(c);
   }
 
-  class MathOperation {
-    public void acceptCalculator(Calculator ref) {
-      ref.multiply();
-      ref.divide();
+  public void divide() {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = 0;
+    if (a == 0 || b == 0) {
+      System.out.println("Please provide non-zero integers.");
+      ;
+    } else {
+      c = a / b;
     }
+
+    System.out.println(c);
   }
-  
+}
+
+class MathOperation {
+  public void acceptCalculator(Calculator ref) {
+    ref.multiply();
+    ref.divide();
+  }
 }
 
 public class InterfaceLesson extends Object {
   public static void main(String[] args) {
-    
+    MyCal1 mc1 = new MyCal1();
+    MyCal2 mc2 = new MyCal2();
+    MyCal3 mc3 = new MyCal3();
+
+    MathOperation mathOp1 = new MathOperation();
+    mathOp1.acceptCalculator(mc1);
+    mathOp1.acceptCalculator(mc2);
+    mathOp1.acceptCalculator(mc3);
+
   }
 
 }
