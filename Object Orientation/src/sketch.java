9207e2eeef1297811fc8631d3732db8c1d5befc5
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-16 12:12:04 +0800

    update lesson

--- Object Orientation/src/sketch.java
+++ Object Orientation/src/sketch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class DemoOne {
 
   public void alpha() {
-    System.out.println("Connection 1 Established");
+    System.out.println("Connection 1 is established");
 
     Scanner scan = new Scanner(System.in);
 
