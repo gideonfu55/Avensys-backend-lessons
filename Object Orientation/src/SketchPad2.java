c5bc2a1a12c543d0dbc78da34d7fcdb6a8c049e4
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-17 09:50:42 +0800

    update Exception Handling - .getMessage()

--- Object Orientation/src/SketchPad2.java
+++ Object Orientation/src/SketchPad2.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
-class DemoOne {
+class DemoTwo {
   public void alpha() {
-
-    System.out.println("Connection 1 is established.");
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Enter the numerator");
-    int a = scan.nextInt();
-    System.out.println("Enter the denominator");
-    int b = scan.nextInt();
-    int c = a / b;
-    System.out.println("Result is: " + c);
-    System.out.println("Connection 1 is closed.");
+    try {   
+      System.out.println("Connection 1 is established.");
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter the numerator");
+      int a = scan.nextInt();
+      System.out.println("Enter the denominator");
+      int b = scan.nextInt();
+      int c = a / b;
+      System.out.println("Result is: " + c);
+      System.out.println("Connection 1 is closed.");
+    }
+    catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
 
 public class SketchPad2 {
   public static void main(String[] args) {
     System.out.println("Main Connection is established");
-    DemoOne d1 = new DemoOne();
+    DemoTwo d1 = new DemoTwo();
     d1.alpha();
     System.out.println("Main Connection is closed.");
   }
