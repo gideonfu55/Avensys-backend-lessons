72c4880a4a6157255c26b0514d44993b519b9b3a
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-17 10:10:21 +0800

    add Exception Rethrowing lesson

--- Object Orientation/src/SketchPad.java
+++ Object Orientation/src/SketchPad.java
@@ -1,17 +1,31 @@
-class Bank {
+import java.util.Scanner;
+
+class ATM {
   public void alpha() {
     try {
-      System.out.println("Connection 3 established.");
-      alpha();
+      System.out.println("Connection 1 is established.");
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter the numerator");
+      int a = scan.nextInt();
+      System.out.println("Enter the denominator");
+      int b = scan.nextInt();
+      int c = a / b;
+      System.out.println("Result is: " + c);
+      System.out.println("Connection 1 is closed.");
+
     } catch (Error e) {
-      System.out.println("Don't keep alpha method in loop");
+      System.out.println("A problem occurred during the input.");
     }
-    System.out.println("Connection 3 is Closed");
+    System.out.println("Connection 1 is Closed");
   }
 }
 
 class SketchPad {
   public static void main(String[] args) {
-    
+    System.out.println("Main connection is established.");
+    ATM atm1 = new ATM();
+    atm1.alpha();
+    System.out.println("Main connection is closed.");
+
   }
 }
