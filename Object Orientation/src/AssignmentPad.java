38a19dadd5833415b5280c397a03156fec3ae38c
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-17 12:41:09 +0800

    update assignment solution for Exception Handling (Custom)

--- Object Orientation/src/AssignmentPad.java
+++ Object Orientation/src/AssignmentPad.java
@@ -1,3 +1,62 @@
-public class AssignmentPad {
-  
+import java.util.Scanner;
+
+// Custom exception:
+class OverAgeException extends Exception {
+  public String getMessage() {
+    return "Your age is above 45 and beyond the age limit.";
+  }
 }
+
+class UnderAgeException extends Exception {
+  public String getMessage() {
+    return "Your age is under 18 and below the age limit.";
+  }
+}
+
+class MatchFinderApp {
+  String username;
+  int ageInput;
+  int overageLimit = 45;
+  int underageLimit = 18;
+
+  public void acceptInput() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter your name: ");
+    username = scan.nextLine();
+    System.out.println("Please enter your age: ");
+    ageInput = scan.nextInt();
+  }
+
+  public void verify() throws OverAgeException, UnderAgeException{
+    if (ageInput >= underageLimit && ageInput <= overageLimit) {
+      System.out.println("Thank you. Please proceed with our match finder services.");
+    } else if (ageInput < underageLimit) {
+      UnderAgeException uae = new UnderAgeException();
+      throw uae;
+    } else if (ageInput > overageLimit) {
+      OverAgeException oae = new OverAgeException();
+      throw oae;
+    }
+  }
+}
+
+class SocialSpace {
+  public void join() {
+    MatchFinderApp newApp1 = new MatchFinderApp();
+    try {
+      newApp1.acceptInput();
+      newApp1.verify();
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
+
+public class AssignmentPad {
+  public static void main(String[] args) {
+    SocialSpace newSS = new SocialSpace();
+    newSS.join();
+  }
+}
+
+
