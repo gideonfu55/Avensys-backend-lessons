3a00d441edf85425a797a758601645bbe6454481
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-19 13:10:04 +0800

    update HashSet lesson example

--- Object Orientation/src/HashSets.java
+++ Object Orientation/src/HashSets.java
@@ -2,12 +2,11 @@ import java.util.HashSet;
 
 public class HashSets {
   public static void main(String[] args) {
-    HashSet<Integer> hs1 = new HashSet<Integer>();
+    HashSet<Integer> hs1 = new HashSet<Integer>(); // Hashing algorithm is provided.
     hs1.add(10);
     hs1.add(20);
     hs1.add(30);
-    System.out.println(hs1);
+    System.out.println(hs1); // insertion order will not be preserved
 
-    
   }
 }
