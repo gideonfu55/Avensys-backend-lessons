99893080246856825f579b4407ff29ceb33829cd
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-19 12:19:45 +0800

    add TreeSets lesson

--- Object Orientation/src/TreeSets.java
+++
@@ -1,5 +0,0 @@
-public class TreeSets {
-  public static void main(String[] args) {
-    
-  }
-}
---
+++ Object Orientation/src/TreeSetsLesson.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class TreeSetsLesson {
+  public static void main(String[] args) {
+    TreeSet<Integer> ts = new TreeSet<Integer>();
+    ts.add(10);
+    ts.add(20);
+    ts.add(30);
+    ts.add(40);
+
+    System.out.println(ts.ceiling(40));
+    System.out.println(ts.floor(10));
+    System.out.println(ts.higher(40));
+    System.out.println(ts.lower(10));
+  }
+}
