66d74b6634be969c72c0b75a316e53a5e6a2e789
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-30 12:43:47 +0800

    add requestmapping to loginController

--- springboot/todolist/src/main/java/com/demo/springboot/todolist/controller/login/LoginController.java
+++ springboot/todolist/src/main/java/com/demo/springboot/todolist/controller/login/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -26,5 +27,12 @@ public class LoginController {
   public String logoutTwo(@PathVariable("name") String name, ModelMap model) {
     model.put("name", name);
     return "logout";
+  }
+
+  // For request param:
+  @RequestMapping("welcome")
+  public String welcome(@RequestParam String traineeName, ModelMap model) {
+    model.put("traineeName", traineeName);
+    return "welcome";
   }
 }
