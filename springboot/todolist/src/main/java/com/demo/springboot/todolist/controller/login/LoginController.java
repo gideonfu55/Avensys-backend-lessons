607df06fff891b535bb5f5523fb173f85b8a6aa6
jcmonkey87 <fuzhonglong@gmail.com>
2023-05-30 11:37:29 +0800

    Add logout method for request param mapping

--- springboot/todolist/src/main/java/com/demo/springboot/todolist/controller/login/LoginController.java
+++ springboot/todolist/src/main/java/com/demo/springboot/todolist/controller/login/LoginController.java
@@ -1,6 +1,9 @@
 package com.demo.springboot.todolist.controller.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +14,17 @@ public class LoginController {
     return "login";
   }
 
+  // Introducing model.addAttribute
+  @RequestMapping("logout/{name}")
+  public String logout(@PathVariable("name") String name, Model model) {
+    model.addAttribute("name", name);
+    return "logout";
+  }
+
+  // OR use ModelMap
+  @RequestMapping("logouttwo/{name}")
+  public String logoutTwo(@PathVariable("name") String name, ModelMap model) {
+    model.put("name", name);
+    return "logout";
+  }
 }
---
+++ springboot/todolist/src/main/resources/META-INF/resources/WEB-INF/jsp/logout.jsp
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Login Page</title>
+  </head>
+  <body>
+    <h1>Hi ${name}, you have logged out successfully!</h1>
+  </body>
+</html>
